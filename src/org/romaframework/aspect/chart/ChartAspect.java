7546b13c53badbb07e870ad8dba6413778f7bdb4
tglman <tglman@tglman.com>
2011-07-25 15:06:44 +0200

    Removed old unused class.

--- src/org/romaframework/aspect/chart/ChartAspect.java
+++ src/org/romaframework/aspect/chart/ChartAspect.java
@@ -1,8 +1,28 @@
 package org.romaframework.aspect.chart;
 
+import java.io.OutputStream;
+
 import org.romaframework.core.aspect.Aspect;
 
 public interface ChartAspect extends Aspect {
+
+	/**
+	 * Render an object as chart.
+	 * 
+	 * @param obj
+	 *          the object to render.
+	 * @return the byte array correspondent to chart.
+	 */
 	public byte[] toChart(Object obj);
+
+	/**
+	 * Render an object as chart.
+	 * 
+	 * @param obj
+	 *          the object to render.
+	 * @param outputStream
+	 *          where write chart.
+	 */
+	public void toChart(Object obj, OutputStream outputStream);
 
 }
