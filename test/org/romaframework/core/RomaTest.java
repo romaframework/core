69435442eab85259bd1dee3f61679790c6c221b3
Tglman <tglman@tglman.com>
2012-02-07 15:23:05 +0100

    make test envoironment artifact.

--- test/org/romaframework/core/RomaTest.java
+++ test/org/romaframework/core/RomaTest.java
@@ -12,13 +12,17 @@ public class RomaTest {
 	}
 
 	public static void initEnvironment(String environment, String... additionalApplicationContext) {
-		String appPath = new File("WebContent").getAbsolutePath();
+		String appPath = new File(".").getAbsolutePath();
 		SpringComponentEngine sce = new SpringComponentEngine();
 		sce.setBasePath(environment);
 		sce.setAdditionalPaths(additionalApplicationContext);
 		RomaApplicationContext.getInstance().setComponentAspect(sce);
 		RomaApplicationContext.setApplicationPath(appPath.substring(0, appPath.length() - 1));
 		RomaApplicationContext.getInstance().startup();
+	}
+
+	public static void destroy() {
+		RomaApplicationContext.getInstance().shutdown();
 	}
 
 }
