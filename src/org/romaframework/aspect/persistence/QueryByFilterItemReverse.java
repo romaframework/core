e6b4871f0f235c3bafe517fde2f14adbc8e4c12f
Tglman <tglman@tglman.com>
2012-02-24 16:21:37 +0100

    add persistence reverse item.

---
+++ src/org/romaframework/aspect/persistence/QueryByFilterItemReverse.java
@@ -0,0 +1,48 @@
+package org.romaframework.aspect.persistence;
+
+import org.romaframework.aspect.persistence.QueryByFilter;
+import org.romaframework.aspect.persistence.QueryByFilterItem;
+import org.romaframework.aspect.persistence.QueryOperator;
+
+/**
+ * An item that allow to manage inverse connection as direct connection. It contains a subQuery for rebuild the original query.
+ * <p>
+ * 
+ * @author Dario Albani
+ */
+public class QueryByFilterItemReverse implements QueryByFilterItem {
+	private QueryByFilter queryByFilter;
+	private String field;
+	private QueryOperator operator;
+
+	public QueryByFilterItemReverse(QueryByFilter queryByFilter, String field, QueryOperator operator) {
+		this.setQueryByFilter(queryByFilter);
+		this.setField(field);
+		this.setOperator(operator);
+	}
+
+	public void setQueryByFilter(QueryByFilter queryByFilter) {
+		this.queryByFilter = queryByFilter;
+	}
+
+	public QueryByFilter getQueryByFilter() {
+		return queryByFilter;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setOperator(QueryOperator operator) {
+		this.operator = operator;
+	}
+
+	public QueryOperator getOperator() {
+		return this.operator;
+	}
+
+}
