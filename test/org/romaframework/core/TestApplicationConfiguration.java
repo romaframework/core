b9d4dd1940a3d0f7511923adaa178d9d545ec827
tglman <tglman@tglman.com>
2012-09-20 16:02:08 +0200

    improvemente of java 1.5 compatibility.

--- test/org/romaframework/core/TestApplicationConfiguration.java
+++ test/org/romaframework/core/TestApplicationConfiguration.java
@@ -4,20 +4,16 @@ import org.romaframework.core.config.AbstractApplicationConfiguration;
 
 public class TestApplicationConfiguration extends AbstractApplicationConfiguration {
 
-	@Override
 	public void startUserSession() {
 	}
 
-	@Override
 	public void endUserSession() {
 	}
 
-	@Override
 	public void startup() throws RuntimeException {
 
 	}
 
-	@Override
 	public void shutdown() throws RuntimeException {
 	}
 
